cb3d548f6dbf0cbc02add86cb96327d663f928b7
Demian Rymar <ua.demyan@gmail.com>
2024-04-05 00:32:46 +0300

    Fixed

--- LAB_10.2/Main.cpp
+++ LAB_10.2/Main.cpp
@@ -6,11 +6,9 @@
 using namespace std;
 
 void processFile(const string& inputFileName, const string& outputFileName) {
-    // Opening files for reading and writing
     ifstream inputFile(inputFileName);
     ofstream outputFile(outputFileName);
 
-    // Check if files were opened successfully
     if (!inputFile.is_open()) {
         cout << "Failed to open the input file " << inputFileName << endl;
         return;
@@ -21,24 +19,19 @@ void processFile(const string& inputFileName, const string& outputFileName) {
         return;
     }
 
-    // Reading lines from the input file
     string line;
     while (getline(inputFile, line)) {
-        // Finding the first space in the line
         size_t spacePos = line.find(' ');
         if (spacePos != string::npos) {
-            // Writing characters before the first space to the output file
             for (size_t i = 0; i < spacePos; ++i) {
                 if (isalpha(line[i])) {
                     outputFile << line[i];
                 }
             }
-            // End the loop as the first space is found
             break;
         }
     }
 
-    // Closing files
     inputFile.close();
     outputFile.close();
 
@@ -46,7 +39,6 @@ void processFile(const string& inputFileName, const string& outputFileName) {
 }
 
 int main() {
-    // File name input
     string inputFileName, outputFileName;
     cout << "Enter the name of the input file: ";
     cin >> inputFileName;
